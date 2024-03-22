4e7841da27359381a46bc481023ac4c748e0e86c
Cosmin Tanislav <demonsingur@gmail.com>
2024-03-22 04:33:55 +0200

    publisher: refactor for new format

--- publish_builds.py
+++ publish_builds.py
@@ -2,9 +2,10 @@
 import json
 import os
 import sys
+import shutil
 
-from file_utils import is_device, is_build, is_other_regex, is_recovery, find_recovery_path_for_build_path
-from publisher import Publisher
+from file_utils import is_device, is_build
+from publisher import LocalPublisher, GithubPublisher
 
 
 if len(sys.argv) < 2:
@@ -33,47 +34,57 @@ def get_config(name, default=None):
 
 
 builds_path = get_config('builds_path')
+ignored_versions = get_config('ignored_versions', [])
 builds_json_path = get_config('builds_json_path')
 builds_limit = get_config('builds_limit', 0)
 github_token = get_config('github_token', '')
 blacklisted_devices = get_config('blacklisted_devices', [])
-whitelisted_files_regex = get_config('whitelisted_files_regex', [])
 
 old_build_paths = []
 new_build_paths = []
-other_file_paths = []
-recovery_paths = []
+
+def try_int(s):
+    try:
+        return int(s)
+    except:
+        return s
+
 devices = [file.path for file in os.scandir(builds_path) if is_device(file, blacklisted_devices)]
 for device in devices:
-    device_build_paths = [file.path for file in os.scandir(device) if is_build(file)]
-    device_build_paths.sort()
+    device_build_paths = [file.path for file in os.scandir(device)]
+    device_build_paths.sort(key=lambda x: try_int(x))
 
     old_build_paths.extend(device_build_paths[:-builds_limit])
     new_build_paths.extend(device_build_paths[-builds_limit:])
 
-    _recovery_paths = [file.path for file in os.scandir(device) if is_recovery(file)]
-    recovery_paths.extend(_recovery_paths)
-
-    _other_file_paths = [file.path for file in os.scandir(device) if is_other_regex(file, whitelisted_files_regex)]
-    other_file_paths.extend(_other_file_paths)
-
 for build_path in old_build_paths:
     build_name = os.path.basename(build_path)
     print(f'removing old build {build_name} from disk')
-    os.remove(build_path)
+    shutil.rmtree(build_path)
 
 if github_token == '':
-    publisher = Publisher.create_local_publisher(builds_path, builds_json_path)
+    publisher = LocalPublisher(builds_path, builds_json_path, ignored_versions)
 else:
-    publisher = Publisher.create_github_publisher(github_token, builds_json_path)
+    publisher = GithubPublisher(github_token, builds_json_path, ignored_versions)
 
 for build_path in new_build_paths:
-    recovery_path = find_recovery_path_for_build_path(build_path, recovery_paths)
-    build = publisher.add_build_from_path(build_path, recovery_path)
-    publisher.upload_build(build)
+    build_files = [file.path for file in os.scandir(build_path)]
 
-for other_path in other_file_paths:
-    other = publisher.add_file_from_path(other_path)
-    publisher.upload_file(other)
+    if not build_files:
+        continue
+
+    rom_path = None
+    extra_paths = []
+    for build_file in build_files:
+        if is_build(build_file):
+            rom_path = build_file
+        else:
+            extra_paths.append(build_file)
+
+    if not rom_path:
+        print(f'found build {build_path} without ROM')
+        continue
+
+    publisher.add_build_from_path(rom_path, extra_paths)
 
 print('completed')
--- publisher.py
+++ publisher.py
@@ -9,6 +9,8 @@ from github import Github
 from datetime import datetime
 from time import mktime
 
+from file_utils import extract_filename_parts
+
 
 def get_sha256(path):
     sha256 = hashlib.sha256()
@@ -22,13 +24,21 @@ def get_sha256(path):
     return sha256.hexdigest()
 
 
+def split_raw_date(raw_date):
+    return '{}-{}-{}'.format(raw_date[0:4], raw_date[4:6], raw_date[6:8])
+
+
+def unix_raw_rate(raw_date):
+    time = datetime.strptime(raw_date, '%Y%m%d')
+    return int(mktime(time.timetuple()))
+
 class BaseFile:
     def __init__(self, path=None, serialization=None):
         if path is not None:
             self.path = path
             self.url = None
             self.size = os.path.getsize(path)
-            self.sha256 = get_sha256(path)
+            self.sha256 = get_sha256(self.path)
             self.filename = os.path.basename(path)
         elif serialization is not None:
             self.path = None
@@ -37,222 +47,197 @@ class BaseFile:
             self.sha256 = serialization['sha256']
             self.filename = serialization['filename']
 
-        self.name = os.path.splitext(self.filename)[0]
+    def __eq__(self, other):
+        if self.path != other.path:
+            return False
 
-        _, version, raw_date, type_, device = self.name.split('-')
-        self.version = version
-        self.type = type_.lower()
-        self.device = device
+        if self.size != other.size:
+            return False
 
-        self.date = '{}-{}-{}'.format(raw_date[0:4], raw_date[4:6], raw_date[6:8])
-        self.datetime = int(mktime(datetime.strptime(raw_date, '%Y%m%d').timetuple()))
+        if self.sha256 != other.sha256:
+            return False
+
+        return True
 
     def serialize(self):
         serialization = {
-            'date': self.date,
-            'datetime': self.datetime,
             'filename': self.filename,
             'filepath': self.url,
             'sha256': self.sha256,
             'size': self.size,
+        }
+
+        return serialization
+
+class RomFile(BaseFile):
+    def __init__(self, path=None, serialization=None):
+        super().__init__(path, serialization)
+
+        name, parts = extract_filename_parts(self.filename)
+
+        self.name = name
+        self.version = parts[1]
+        raw_date = parts[2]
+        self.type = parts[3].lower()
+        self.device = parts[4]
+
+        self.date = split_raw_date(raw_date)
+        self.datetime = unix_raw_rate(raw_date)
+
+class Build:
+    def __init__(self, files=None, serialization=None):
+        if files is not None:
+            rom_file = files[0]
+            self.files = files
+            self.type = rom_file.type
+            self.version = rom_file.version
+            self.date = rom_file.date
+            self.datetime = rom_file.datetime
+        elif serialization is not None:
+            self.files = [BaseFile(file_serialization) for file_serialization in serialization['files']]
+            self.type = serialization['type']
+            self.version = serialization['version']
+            self.date = serialization['date']
+            self.datetime = serialization['datetime']
+
+    def __eq__(self, other):
+        return self.files == other.files
+
+    def serialize(self):
+        serialization = {
             'type': self.type,
             'version': self.version,
+            'date': self.date,
+            'datetime': self.datetime,
+            'files': [file.serialize() for file in self.files]
         }
 
         return serialization
 
 
-class Build(BaseFile):
-    def __init__(self, path=None, recovery_path=None, serialization=None):
-        super().__init__(path, serialization)
-
-        if path is not None:
-            if recovery_path is not None:
-                self.recovery = BaseFile(path=recovery_path)
-            else:
-                self.recovery = None
-        elif serialization is not None:
-            if 'recovery' in serialization:
-                self.recovery = BaseFile(serialization=serialization['recovery'])
-            else:
-                self.recovery = None
-
-    def serialize(self):
-        serialization = super().serialize()
-
-        if self.recovery:
-            serialization['recovery'] = self.recovery.serialize()
-
-        return serialization
-
-
-class FileFactory:
-    @staticmethod
-    def from_serialization(serialization):
-        if serialization['type'] == 'unofficial':
-            file = Build(serialization=serialization)
-        else:
-            file = BaseFile(serialization=serialization)
-
-        return file
-
-
 class Publisher:
-    def __init__(self, upload_fn, builds_json_path):
-        self.__upload_fn = upload_fn
+    def __init__(self, builds_json_path, ignored_versions):
         self.__builds_json_path = builds_json_path
-        self.__files = []
+        self.__ignored_versions = ignored_versions
+        self.__devices = []
 
-        self.read()
+        self._read()
 
-    def find_file_with_filename(self, filename):
-        for file in self.__files:
-            if file.filename == filename:
-                return file
+    def add_build_from_path(self, rom_path, extra_paths):
+        rom_file = RomFile(rom_path)
+        if rom_file.version in self.__ignored_versions:
+            return
 
-        return None
+        files = [rom_file]
+        for extra_path in extra_paths:
+            extra_file = BaseFile(extra_path)
+            files.append(extra_file)
 
-    def upload_file(self, file):
-        self.__upload_fn(file)
-        self.write()
+        build = Build(files=files)
+        device = rom_file.device
 
-    def upload_build(self, build):
-        self.upload_file(build)
+        if device not in self.__devices:
+            self.__devices[device] = []
 
-        if build.recovery is not None:
-            self.upload_file(build.recovery)
+        builds = self.__devices[device]
 
-    def __add_file(self, new_file):
-        existing_file = self.find_file_with_filename(new_file.filename)
+        if build in builds:
+            return
 
-        print(f'adding file {new_file.filename}')
+        self._upload_build(build)
 
-        # If a file with the same name does not already exist
-        if existing_file is None:
-            # Then add the new file to the list
-            # and return it
-            self.__files.append(new_file)
-            return new_file
-        else:
-            print('\tfound existing file')
+        builds.append(build)
 
-        # If the existing file the same sha256 as the old file
-        # return the existing file
-        if existing_file.sha256 == new_file.sha256:
-            print('\texisting file is the same as new file, using existing file')
-            return existing_file
+        self._write()
 
-        # Otherwise, remove the existing file from the list,
-        # add the new file, and return it
-        print('\texisting file is outdated')
-        self.__files.remove(existing_file)
-        self.__files.append(new_file)
-        return new_file
+    def _upload_build(self, build):
+        pass
 
-    def add_file_from_path(self, file_path):
-        new_file = BaseFile(path=file_path)
-        return self.__add_file(new_file)
-
-    def add_build_from_path(self, build_path, recovery_path=None):
-        new_build = Build(path=build_path, recovery_path=recovery_path)
-        existing_build = self.find_file_with_filename(new_build.filename)
-
-        # If the existing build does not have a recovery
-        # or if the new build doesn't have a recovery,
-        # or if the new build has a different recovery,
-        # set the existing build's recovery to the new one
-        # Otherwise, set the new build's recovery to the
-        # old one, which is already uploaded
-        # This provides parity recovery-wise, so that
-        # no matter which of the 2 builds will be kept
-        # both will have the same recovery
-        if existing_build is not None:
-            if existing_build.recovery is None or new_build.recovery is None or \
-                    existing_build.recovery.sha256 != new_build.recovery.sha256:
-                print('\trecovery has been added, changed, or removed, using new recovery')
-                existing_build.recovery = new_build.recovery
-            else:
-                print('\texisting recovery is the same as new recovery, using existing recovery')
-                new_build.recovery = existing_build.recovery
-
-        return self.__add_file(new_build)
-
-    def read(self):
+    def _read(self):
         # Read data from the builds.json file
         try:
             with open(self.__builds_json_path, 'r') as builds_json_file:
-                devices_data = json.load(builds_json_file)
+                devices_serialization = json.load(builds_json_file)
         except IOError:
-            devices_data = {}
+            devices_serialization = {}
 
         # Deserialize files
-        for device, serializations in devices_data.items():
-            files = [FileFactory.from_serialization(serialization)
-                     for serialization in serializations]
-            self.__files.extend(files)
+        for device, builds_serialization in devices_serialization.items():
+            builds = [Build(build_serialization) for build_serialization in builds_serialization]
+            self.__devices[device] = builds
 
-    def write(self):
-        # Group files by device
-        devices = {}
-        for file in self.__files:
-            if file.device not in devices:
-                devices[file.device] = []
-
-            devices[file.device].append(file)
-
+    def _write(self):
         # Serialize files
-        devices_data = {}
-        for device, files in devices.items():
-            files.sort(key=lambda b: b.date)
-            devices_data[device] = [file.serialize() for file in files]
+        devices_serialization = {}
+        for device, builds in self.__devices.items():
+            builds_serialization = [build.serialize() for build in builds]
+            devices_serialization[device] = builds_serialization
 
         # Write data back into the builds.json file
         with open(self.__builds_json_path, 'w') as builds_json_file:
-            json.dump(devices_data, builds_json_file)
+            json.dump(devices_serialization, builds_json_file)
 
-    @staticmethod
-    def create_github_publisher(github_token, builds_json_path):
-        github_user = Github(github_token).get_user()
+class LocalPublisher(Publisher):
+    def __init__(self, base_path, builds_json_path, ignored_versions):
+        super().__init__(builds_json_path, ignored_versions)
 
-        def upload_file_fn(file):
-            # File is already uploaded
-            if file.url is not None:
-                print(f'file {file.filename} already uploaded')
-                return
+        self.base_path = base_path
 
-            # Try to create the repo, if it exists, skip the creation
-            try:
-                repo = github_user.create_repo(file.device)
-                repo.create_file('README', 'initial commit', file.device)
-            except:
-                repo = github_user.get_repo(file.device)
+    def _upload_build(self, build):
+        for file in build.files:
+            file.url = relpath(file.path, self.base_path)
 
-            # Try to delete the release if it exists
-            try:
-                release = repo.get_release(file.name)
-                release.delete_release()
-            except:
-                pass
+class GithubPublisher(Publisher):
+    def __init__(self, github_token, builds_json_path, ignored_versions):
+        super().__init__(builds_json_path, ignored_versions)
 
-            # Create the release
-            release = repo.create_git_release(file.name, file.name, file.name)
-            release.upload_asset(file.path)
+        self.github_user = Github(github_token).get_user()
 
-            print(f'uploaded file {file.filename}')
+    def _get_repo(self, build):
+        file = build.files[0]
 
-            github_username = github_user.login
-            file.url = 'https://github.com/{}/{}/releases/download/{}/{}' \
-                .format(github_username, file.device, file.name, file.filename)
+        # Try to create the repo, if it exists, skip the creation
+        try:
+            repo = self.github_user.create_repo(build.device)
+            repo.create_file('README', 'initial commit', file.device)
+        except:
+            repo = self.github_user.get_repo(file.device)
 
-        return Publisher(upload_file_fn, builds_json_path)
+        return repo
 
-    @staticmethod
-    def create_local_publisher(base_path, builds_json_path):
-        def extract_relative_path_fn(file):
-            # File is already processed
-            if file.url is not None:
-                return
+    def _create_release(self, repo, build):
+        file = build.files[0]
 
-            file.url = relpath(file.path, base_path)
+        # Try to delete the release if it exists
+        try:
+            release = repo.get_release(file.name)
+            release.delete_release()
+        except:
+            pass
 
-        return Publisher(extract_relative_path_fn, builds_json_path)
+        # Create the release
+        release = repo.create_git_release(file.name, file.name, file.name)
+
+        return release
+
+    def _upload_file(self, release, file):
+        print(f'uploading file {file.filename}')
+
+        release.upload_asset(file.path)
+
+        print(f'uploaded file {file.filename}')
+
+        github_username = self.github_user.login
+        file.url = 'https://github.com/{}/{}/releases/download/{}/{}' \
+            .format(github_username, file.device, file.name, file.filename)
+
+    def _upload_build(self, build):
+        repo = self._get_repo(build)
+        release = self._create_release(repo, build)
+
+        file = build.files[0]
+
+        print(f'uploading build {file.filename}')
+
+        for file in build.files:
+            self._upload_file(release, file)
